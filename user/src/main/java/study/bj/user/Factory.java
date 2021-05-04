824ab536ccbbdae57d84699d89afe101cee09936
Renoris <55519101+Renoris@users.noreply.github.com>
2021-05-04 18:03:50 +0900

    IOC- path관련

--- user/src/main/java/study/bj/user/Factory.java
+++ user/src/main/java/study/bj/user/Factory.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.nio.charset.Charset;
+import javax.servlet.ServletContext;
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
@@ -17,6 +19,13 @@ public class Factory {
     @Bean
     public HttpMessageConverter<String> responseBodyConverter(){
         return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+    }
+
+    @Bean
+    public String clientImagePath(ServletContext context){
+//        String absolutePath=context.getRealPath("/");
+//        return absolutePath+"images/clientImages/";
+        return "F:\\Desktop\\imageSave\\";
     }
 
 }
