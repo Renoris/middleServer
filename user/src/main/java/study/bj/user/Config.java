75609d07b581f1a566fb40971c99ddcdffb1268c
Renoris <55519101+Renoris@users.noreply.github.com>
2021-05-10 12:48:32 +0900

    혹시모르니 추가

--- user/src/main/java/study/bj/user/Config.java
+++ user/src/main/java/study/bj/user/Config.java
@@ -1,12 +1,24 @@
 package study.bj.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class Config implements WebMvcConfigurer{
+@ComponentScan("study.bj.user")
+public class Config extends WebMvcConfigurationSupport {
 
+    @Autowired
+    String clientImagePath;
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations(clientImagePath).setCachePeriod(31536000);
+    }
 }
